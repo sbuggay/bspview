02f8bbbb8a5ccfbf9d02d7ef35a97616f45c570e
sbuggay <sbuggay@gmail.com>
2020-03-20 12:24:01 -0700

    Add filedrop support

--- src/MapSelector.ts
+++ src/MapSelector.ts
@@ -1,29 +1,29 @@
 export const maps = [
-    "c1a0.bsp",
-    "c1a0a.bsp",
-    "c1a0b.bsp",
-    "c1a0c.bsp",
-    "c1a0e.bsp",
-    "c1a0e.bsp",
-    "c1a1.bsp",
-    "c1a1a.bsp",
-    "c1a1b.bsp",
-    "c1a1c.bsp",
-    "c1a1d.bsp",
-    "c1a1f.bsp",
+    "halflife_c1a0.bsp",
+    "halflife_c1a0a.bsp",
+    "halflife_c1a0b.bsp",
+    "halflife_c1a0c.bsp",
+    "halflife_c1a0e.bsp",
+    "halflife_c1a0e.bsp",
+    "halflife_c1a1.bsp",
+    "halflife_c1a1a.bsp",
+    "halflife_c1a1b.bsp",
+    "halflife_c1a1c.bsp",
+    "halflife_c1a1d.bsp",
+    "halflife_c1a1f.bsp",
 ]
 
 
 export class MapSelector {
 
-    select: HTMLSelectElement;
+    public select: HTMLSelectElement;
 
     constructor(element: HTMLElement, callback: ((evt: Event) => any)) {
         this.select = document.createElement("select");
         this.select.style.position = "absolute";
         this.select.style.top = "0";
         this.select.style.right = "0";
-        this.select.style.width = "200px";
+        this.select.style.width = "300px";
         this.select.style.fontSize = "1.5em";
 
         maps.forEach(map => {
--- src/entry.ts
+++ src/entry.ts
@@ -5,11 +5,8 @@ import * as Stats from "stats.js";
 import { FlyControls } from "./FlyControls";
 import { MapSelector, maps } from "./MapSelector";
 
-
-
 const clock = new THREE.Clock();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 3000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -18,87 +15,114 @@ window.onresize = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-new MapSelector(document.body, (event) => {
+const mapSelector = new MapSelector(document.body, (event) => {
     const value = (event.target as HTMLSelectElement).value;
-    loadMap(value);
+    const url = `https://devanbuggay.com/bspview/bsp/${value}`;
+    loadMapFromUrl(url);
 });
 
-function loadMap(map: string) {
+function registerDragEvents() {
+    ["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+        document.body.addEventListener(eventName, preventDefaults, false);
+    })
+
+    function preventDefaults(event: Event) {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+
+    // document.body.addEventListener("dragenter", handlerFunction, false);
+    // document.body.addEventListener("dragleave", handlerFunction, false);
+    // document.body.addEventListener("dragover", handlerFunction, false);
+    document.body.addEventListener("drop", drop, false);
+
+    function drop(event: DragEvent) {
+        let dt = event.dataTransfer;
+        let files = dt.files;
+        const file = files[0];
+
+        if (file) {
+            (file as any).arrayBuffer().then((buffer: ArrayBuffer) => {
+                loadMap(buffer);
+            });
+        }
+        else {
+            console.error("No file found!");
+        }
+    }
+}
+
+async function loadMapFromUrl(url: string) {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    loadMap(buffer);
+}
+
+function loadMap(buffer: ArrayBuffer) {
 
     let scene = new THREE.Scene();
 
-    const url = `https://devanbuggay.com/bspview/bsp/${map}`;
+    const bsp = parseBSP(buffer);
+    var geometry = new THREE.Geometry();
 
-    fetch(url).then(async (response) => {
-        const buffer = await response.arrayBuffer();
-        const bsp = parseBSP(buffer);
-        var geometry = new THREE.Geometry();
+    bsp.faces.forEach((face) => {
 
-        bsp.faces.forEach((face) => {
+        let firstEdgeIndex = face.firstEdge;
 
-            let firstEdgeIndex = face.firstEdge;
+        for (let i = 0; i < face.edges; i++) {
 
-            for (let i = 0; i < face.edges; i++) {
+            const surfEdge = bsp.surfEdges[firstEdgeIndex + i];
+            const edge = bsp.edges[Math.abs(surfEdge)];
 
-                const surfEdge = bsp.surfEdges[firstEdgeIndex + i];
-                const edge = bsp.edges[Math.abs(surfEdge)];
+            let v1 = bsp.vertices[edge[0]];
+            let v2 = bsp.vertices[edge[1]];
 
-                let v1 = bsp.vertices[edge[0]];
-                let v2 = bsp.vertices[edge[1]];
+            geometry.vertices.push(new THREE.Vector3(v1.y, v1.z, v1.x));
+            geometry.vertices.push(new THREE.Vector3(v2.y, v2.z, v2.x));
 
-                // if (firstVertex === null) {
-                //     firstVertex = v1;
-                // }
+        }
 
-                // if (surfEdge < 0) {
-                //     [v1, v2] = [v2, v1];
-                // }
+        // if (firstVertex) {
+        //     geometry.vertices.push(new THREE.Vector3(firstVertex.y, firstVertex.z, firstVertex.x));
+        // }
 
-                geometry.vertices.push(new THREE.Vector3(v1.y, v1.z, v1.x));
-                geometry.vertices.push(new THREE.Vector3(v2.y, v2.z, v2.x));
+        // const triangles = THREE.ShapeUtils.triangulateShape(geometry.vertices, []);
 
-            }
+        // for (var i = 0; i < triangles.length; i++) {
+        //     geometry.faces.push(new THREE.Face3(triangles[i][0], triangles[i][1], triangles[i][2]));
+        // }
 
-            // if (firstVertex) {
-            //     geometry.vertices.push(new THREE.Vector3(firstVertex.y, firstVertex.z, firstVertex.x));
-            // }
+        // material.side = THREE.DoubleSide;
 
-            // const triangles = THREE.ShapeUtils.triangulateShape(geometry.vertices, []);
-
-            // for (var i = 0; i < triangles.length; i++) {
-            //     geometry.faces.push(new THREE.Face3(triangles[i][0], triangles[i][1], triangles[i][2]));
-            // }
-
-            // material.side = THREE.DoubleSide;
-
-        });
-
-        const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-        const mesh = new THREE.LineSegments(geometry, material);
-        scene.add(mesh);
-
-
-        var stats = new Stats();
-        stats.showPanel(0);
-        document.body.appendChild(stats.dom);
-
-        const controls = new FlyControls(camera, renderer.domElement);
-
-        controls.movementSpeed = 500;
-        controls.domElement = renderer.domElement;
-
-        const render = function () {
-            const delta = clock.getDelta();
-            stats.begin();
-            renderer.render(scene, camera);
-            stats.end();
-            controls.update(delta);
-            requestAnimationFrame(render);
-        };
-
-        render();
     });
+
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const mesh = new THREE.LineSegments(geometry, material);
+    scene.add(mesh);
+
+
+    var stats = new Stats();
+    stats.showPanel(0);
+    document.body.appendChild(stats.dom);
+
+    const controls = new FlyControls(camera, renderer.domElement);
+
+    controls.movementSpeed = 500;
+    controls.domElement = renderer.domElement;
+
+    const render = function () {
+        const delta = clock.getDelta();
+        stats.begin();
+        renderer.render(scene, camera);
+        stats.end();
+        controls.update(delta);
+        requestAnimationFrame(render);
+    };
+
+    render();
+
 }
 
-loadMap(maps[0]);
+registerDragEvents();
+loadMapFromUrl(`https://devanbuggay.com/bspview/bsp/${maps[0]}`);
 
