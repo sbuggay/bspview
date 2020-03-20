c54e3baa81959b4a1e25f07ead591116755ffd5c
sbuggay <sbuggay@gmail.com>
2020-03-19 19:27:28 -0700

    Slow mouselook a tad

--- src/flyControls.ts
+++ src/flyControls.ts
@@ -172,8 +172,8 @@ export class FlyControls {
         if (this.moving) {
             var xAxis = new THREE.Vector3(1, 0, 0);
             var yAxis = new THREE.Vector3(0, 1, 0);
-            this.camera.rotateOnAxis(xAxis, event.movementY * -0.005);
-            this.camera.rotateOnWorldAxis(yAxis, event.movementX * -0.005);
+            this.camera.rotateOnAxis(xAxis, event.movementY * -0.002);
+            this.camera.rotateOnWorldAxis(yAxis, event.movementX * -0.002);
         }
     };
 
